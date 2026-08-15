ed38e21d50f5c3d6318de6d896690c143d88efb2
Álysson Gleyson da Silva <alyssongleyson.dev@gmail.com>
2026-08-15 18:53:08 -0300

    refactor(ssh): modularize port script with functions and input validation

--- workstations/fedora-notebook/scripts/set-ssh-port.sh
+++ workstations/fedora-notebook/scripts/set-ssh-port.sh
@@ -1,5 +1,36 @@
 #!/bin/bash
 
+show_port() {
+	grep -E "^#?Port " /etc/ssh/sshd_config | awk '{print $2}'
+}
+
+change_port() {
+	local port
+	local newport
+
+	port=$(show_port)
+
+	echo "Current Port: $port"
+	read -p "Enter the new port: " newport
+
+	sed -i "s/^#\?Port $port/Port $newport/" /etc/ssh/sshd_config
+
+	mkdir -p /etc/systemd/system/sshd.socket.d/
+
+	echo -e "[Socket]\nListenStream=\nListenStream=$newport" > /etc/systemd/system/sshd.socket.d/override.conf
+
+	semanage port -a -t ssh_port_t -p tcp $newport
+	
+	firewall-cmd --zone=public --add-port=$newport/tcp --permanent
+	firewall-cmd --zone=public --remove-service=ssh --permanent
+	firewall-cmd --reload
+	
+	systemctl daemon-reload
+	systemctl restart sshd.socket sshd
+	systemctl enable sshd.socket
+	systemctl enable sshd.service
+}
+
 while true; do
 	clear
 
@@ -15,28 +46,19 @@ while true; do
 
 	case "$option" in
 		1)
-			grep -E "^#?Port " /etc/ssh/sshd_config | awk '{print $2}'
+			show_port
 			;;
 		2)
-			port=$(grep -E "^#?Port " /etc/ssh/sshd_config | awk '{print $2}')
-			echo "Current Port: " $port
-			read -p "Enter the new port: " newport
-			sed -i "s/^#\?Port $port/Port $newport/" /etc/ssh/sshd_config
-			mkdir -p /etc/systemd/system/sshd.socket.d/
-			echo -e "[Socket]\nListenStream=\nListenStream=$newport" > /etc/systemd/system/sshd.socket.d/override.conf
-			semanage port -a -t ssh_port_t -p tcp $newport
-			firewall-cmd --zone=public --add-port=$newport/tcp --permanent
-			firewall-cmd --zone=public --remove-service=ssh --permanent
-			firewall-cmd --reload
-			systemctl daemon-reload
-			systemctl restart sshd.socket sshd
-			systemctl enable sshd.socket
-			systemctl enable sshd.service
+			change_port
 			;;
 		3)
 			exit 0
-		;;
+			;;
+		*)
+			echo "Invalid option."
+			;;
 	esac
+
 	echo "================================"
 	read -p "Press ENTER to continue..."
 done
